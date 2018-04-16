e96512e0b2710be34b155b8b8c57a3de1d8e39e1
Sarah Murphy <sarahmaeve@users.noreply.github.com>
2018-04-16 11:11:13 -0700

    basic exercises in Go, day 1 morning

--- basics/basic1.go
+++ basics/basic1.go
@@ -8,7 +8,15 @@ import (
 	"math/rand"
 )
 
+// simple func and variable declarations
+func add(x, y float64) float64 {
+  return x + y
+}
+
 func main() {
 	fmt.Println("The square root of 4 is", math.Sqrt(4))
 	fmt.Println("A number from 0 to 99:", rand.Intn(100)) // always 81 without seed
+  var add1 float64 = 10.32
+  add2 := 32.1042
+  fmt.Println("My addition is:", add(add1, add2))
 }
